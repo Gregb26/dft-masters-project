dedf52258eb78f5cecb215076d4456b9fbca88de
gregb26 <gregoire.barrette@umontreal.ca>
2025-06-19 09:32:36 -0400

    added plot.py

---
+++ scripts/plot.py
@@ -0,0 +1,49 @@
+"""
+plot.py
+
+Plots data, usually total_energy vs some parameter to eyeball the best parameter (the one that
+minimizes the total energy).
+
+Usage:
+    python plot.py --param PARAM, in the directory with *GSR.nc file to analyze
+
+Inputs:
+    *GSR.nc files, --param: ecut, nkpt, volume.
+
+Outputs:
+    Plot of total_energy vs param
+
+Dependencies:
+    matplotlib, natsort, abinit_tools.reader
+"""
+
+import matplotlib.pyplot as plt
+import argparse
+import glob
+
+from natsort import natsorted
+from abinit_tools.reader import read_files
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--param", required=True, help="Parameter to extract from file: ecut, volume, nkpt")
+    args = parser.parse_args()
+
+    files = natsorted(glob.glob("*GSR.nc"))
+    energy, params = read_files(files, args.param)
+
+    print(energy)
+    print(params)
+
+    plt.plot(params, energy, "*k")
+    plt.plot(params, energy, "--k")
+    plt.ylabel("Energy (Ha)")
+    plt.xlabel(f"{args.param}")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
